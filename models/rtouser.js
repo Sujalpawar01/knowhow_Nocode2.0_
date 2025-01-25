d0af9ea80b2dcaa435f31a322246de9263a5af0f
Sujalpawar01 <131191220+Sujalpawar01@users.noreply.github.com>
2025-01-25 07:38:13 +0530

    Add files via upload

---
+++ models/rtouser.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  pincode:{type:String},
+  city:{type:String},
+});
+
+const rtouser1 = mongoose.models.rtouser || mongoose.model("rtouser", userSchema);
+
+export default rtouser1;
